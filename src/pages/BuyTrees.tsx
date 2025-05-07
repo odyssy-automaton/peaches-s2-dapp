6a2105d6ac7dbd9e110d5a974414a42737ecd7b0
skuhlmann <samkuhlmann@gmail.com>
2025-05-07 12:08:01 -0600

    remove one fund

--- src/pages/BuyTrees.tsx
+++ src/pages/BuyTrees.tsx
@@ -1,7 +1,6 @@
 import { usePrivy } from "@privy-io/react-auth";
 import {
   NFT_CONTRACT_ADDRESS,
-  NFT_MINT_PRICE,
   NftTreeMeta,
   TARGET_NETWORK,
   TREE_NFT_DATA,
@@ -11,18 +10,12 @@ import { Box, Divider, Flex, Text } from "@chakra-ui/react";
 // import { RemainingTreeSupply } from "../components/RemainingTreeSupply";
 // import { BoostContent } from "../components/BoostContent";
 import { RemainingTreeSupply } from "../components/RemainingTreeSupply";
-import { useBalance, useReadContract } from "wagmi";
+import { useReadContract } from "wagmi";
 
 import erc721Abi from "../abis/ERC721.json";
-import { fromWei } from "../utils/formatting";
-import { FundWallet } from "../components/FundWallet";
 
 function BuyTrees() {
-  const { ready, authenticated, user } = usePrivy();
-
-  const { data } = useBalance({
-    address: user?.wallet?.address as `0x${string}`,
-  });
+  const { user } = usePrivy();
 
   const { data: discountBalance } = useReadContract({
     address: NFT_CONTRACT_ADDRESS[TARGET_NETWORK] as `0x${string}`,
@@ -31,8 +24,6 @@ function BuyTrees() {
     args: [user?.wallet?.address as `0x${string}`],
   }) as { data: bigint };
   const hasDiscount = discountBalance > 0;
-
-  const hasBalance = data && data.value > NFT_MINT_PRICE[TARGET_NETWORK];
 
   return (
     <>
@@ -43,23 +34,6 @@ function BuyTrees() {
       <Box mb="2rem" textAlign="center">
         {/* <Heading size="xl">Tree Sales Have Ended</Heading> */}
       </Box>
-
-      {ready && authenticated && !hasBalance && (
-        <Flex
-          w="100%"
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          mb="2rem"
-        >
-          <Text fontSize="md" textAlign="center">
-            You need{" "}
-            {`${fromWei(NFT_MINT_PRICE[TARGET_NETWORK].toString())} BASE ETH`}{" "}
-            to purchase a tree.
-          </Text>
-          <FundWallet />
-        </Flex>
-      )}
 
       <Flex
         gap="1rem"
